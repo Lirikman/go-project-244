4de578e005da25cc6f90fe3d3f8a20f577b00319
Lirikman <999gothicman999@mail.ru>
2026-03-04 01:00:21 +0700

    incomplete implementation of the comparison function

--- cmd/gendiff/main.go
+++ cmd/gendiff/main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"slices"
 
 	"github.com/urfave/cli/v3"
 )
@@ -35,11 +37,33 @@ func main() {
 			},
 		},
 		Action: func(context.Context, *cli.Command) error {
+			// проверка корректности путей к файлам
+			_, errPath1 := os.Stat(path1)
+			_, errPath2 := os.Stat(path2)
+
+			if errPath1 != nil || errPath2 != nil {
+				return fmt.Errorf("files at the entered path were not found")
+			}
 			if path1 == "" || path2 == "" {
 				return fmt.Errorf("Invalid files path")
 			}
-			parser.ReadData(path1)
-			parser.ReadData(path2)
+
+			// парсинг данных мз файлов
+			data1, _ := parser.ReadData(path1)
+			fileName1 := filepath.Base(path1)
+			fmt.Printf("Файл: %s\n", fileName1)
+			for k, v := range data1 {
+				fmt.Printf("key:%v\tvalue:%v\n", k, v)
+			}
+
+			data2, _ := parser.ReadData(path2)
+			fileName2 := filepath.Base(path2)
+			fmt.Printf("Файл: %s\n", fileName2)
+			for k, v := range data2 {
+				fmt.Printf("key:%v\tvalue:%v\n", k, v)
+			}
+
+			fmt.Println(genDiff(data1, data2))
 			return nil
 		},
 	}
@@ -47,4 +71,48 @@ func main() {
 	if err := cmd.Run(context.Background(), os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func genDiff(dataFile1, dataFile2 map[string]any) string {
+	// переменная для сообщения
+	var res string
+	// переменная для хранения всех уникальных ключей
+	var allUniqueKeys []string
+	// получаем все уникальные ключи из обоих файлов
+	for k := range dataFile1 {
+		allUniqueKeys = append(allUniqueKeys, k)
+	}
+	for k := range dataFile2 {
+		if !slices.Contains(allUniqueKeys, k) {
+			allUniqueKeys = append(allUniqueKeys, k)
+		}
+	}
+	// сортируем ключи по возрастанию
+	slices.Sort(allUniqueKeys)
+	fmt.Println(allUniqueKeys)
+	// проходим по всем ключам и сравниваем
+	// формируем сообщение
+	res += "{\n"
+	for _, keyName := range allUniqueKeys {
+		// получаем ключи и наличие в карте
+		val1, ok1 := dataFile1[keyName]
+		val2, ok2 := dataFile2[keyName]
+		// проверяем условия и фомируем сообщение
+		// наличие в обеих картах и значения совпадают
+		if ok1 && ok2 && val1 == val2 {
+			res += fmt.Sprintf(" %s: %v\n", keyName, val1)
+			// наличие в обеих картах и значения не совпадают
+		} else if ok1 && ok2 && val1 != val2 {
+			res += fmt.Sprintf(" - %s: %v\n", keyName, val1)
+			res += fmt.Sprintf(" + %s: %v\n", keyName, val2)
+			// наличие ключа только в первой карте
+		} else if ok1 && !ok2 {
+			res += fmt.Sprintf(" - %s: %v\n", keyName, val1)
+			// наличие ключа только во второй карте
+		} else if !ok1 && ok2 {
+			res += fmt.Sprintf(" + %s: %v\n", keyName, val2)
+		}
+	}
+	res += ("}\n")
+	return res
 }
--- internal/parsers/parsers.go
+++ internal/parsers/parsers.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"path/filepath"
 	"strings"
 
 	"gopkg.in/yaml.v3"
@@ -20,77 +19,56 @@ func ReadFiles(path string) ([]byte, error) {
 }
 
 // определяем карту для хранения данных
-var parsData map[string]map[string]any
+var parsData map[string]any
+
+// функция для очистки карты
+func clearMap(m map[string]any) {
+	for k := range m {
+		delete(m, k)
+	}
+}
 
 // функция парсинга файлов .json в переменную parsData
 func ReadJson(path string, jsonByte []byte) error {
-	// получаем имя файла
-	fileName := filepath.Base(path)
-	// создаём временную карту для парсинга одного файла
-	var tempData map[string]any
-	//  десериализуем json файл во временную карту
-	err := json.Unmarshal(jsonByte, &tempData)
+	// очищаем карту перед десериализацией json
+	clearMap(parsData)
+	//  десериализуем json файл в карту
+	err := json.Unmarshal(jsonByte, &parsData)
 	if err != nil {
 		return fmt.Errorf("error: %w", err)
 	}
-	// инициализируем карту хранения данных
-	parsData = make(map[string]map[string]any)
-	if parsData[fileName] == nil {
-		parsData[fileName] = make(map[string]any)
-	}
-	// добавляем данные в карту хранения данных
-	parsData[fileName] = tempData
 	return nil
 }
 
 // функция парсинга файлов .yml в переменную parsData
 func ReadYaml(path string, yamlByte []byte) error {
-	// получаем имя файла
-	fileName := filepath.Base(path)
-	// создаём временную карту для парсинга одного файла
-	var tempData map[string]any
-	//  десериализуем yaml файл во временную карту
+	// очищаем карту перед десериализацией нфьд
+	clearMap(parsData)
+	//  десериализуем yaml файл в карту
 	err := yaml.Unmarshal(yamlByte, &parsData)
 	if err != nil {
 		return fmt.Errorf("error: %w", err)
 	}
-	// инициализируем карту хранения данных
-	parsData = make(map[string]map[string]any)
-	if parsData[fileName] == nil {
-		parsData[fileName] = make(map[string]any)
-	}
-	// добавляем данные в карту хранения данных
-	parsData[fileName] = tempData
 	return nil
 }
 
 // функция чтения и парсинга файлов .json и .yaml
-func ReadData(path string) error {
+func ReadData(path string) (map[string]any, error) {
 	var res error
 	data, _ := ReadFiles(path)
 	// проверяем что расширение файла json
 	if strings.HasSuffix(path, ".json") {
 		res = ReadJson(path, data)
-		if res == nil {
-			for name, allData := range parsData {
-				fmt.Printf("Файл: %s\n", name)
-				for k, v := range allData {
-					fmt.Printf("key:%v\tvalue:%v\n", k, v)
-				}
-			}
+		if res != nil {
+			return nil, fmt.Errorf("error: %w", res)
 		}
 	}
 	// проверяем что расширение файла yml
 	if strings.HasSuffix(path, ".yml") {
 		res = ReadYaml(path, data)
-		if res == nil {
-			for name, allData := range parsData {
-				fmt.Printf("Файл: %s\n", name)
-				for k, v := range allData {
-					fmt.Printf("key:%v\tvalue:%v\n", k, v)
-				}
-			}
+		if res != nil {
+			return nil, fmt.Errorf("error: %w", res)
 		}
 	}
-	return res
+	return parsData, nil
 }
